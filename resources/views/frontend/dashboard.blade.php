e99a38f00d59dbb37078da74380ead5848d21b7a
Michael Kachi <jesmikky@gmail.com>
2023-03-31 11:17:54 +0100

    singature added

--- resources/views/frontend/dashboard.blade.php
+++ resources/views/frontend/dashboard.blade.php
@@ -305,11 +305,12 @@
     <div class="container">
         
         <div class="row justify-content-center">
-            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+          @if(count($blogs) > 0)  <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
                 <div class="sec_title position-relative text-center mb-4">
                     <h6 class="text-muted mb-0">Latest Blogs</h6>
                 </div>
             </div>
+            @endif
         </div>
         
         <div class="row justify-content-center">
